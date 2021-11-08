5373f96f0ace975b94bd4911caca320b48c7c1c2
BenjiAho <benjamin.aho-nienne@le-campus-numerique.fr>
2021-11-08 16:17:42 +0100

    finit itération 2

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"main","l":"Arme"},{"p":"main","l":"Guerrier"},{"p":"main","l":"Magicien"},{"p":"main","l":"Menu"},{"p":"main","l":"Spell"}]
